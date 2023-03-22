a6043c1dd9226cb652b0cece114e5a94ebec1308
unknown <oneloveeeeeeant@gmail.com>
2023-03-22 15:54:58 +0200

    fix

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -23,8 +23,8 @@ const Newsletter = () => {
                         <h3>Newsletter</h3>
                         <p>To subscribe to our newsletter, we kindly request your email address. Your email address will be kept confidential and will only be used to send you our newsletter and other relevant updates. We take your privacy seriously and will not share your information with any third parties. </p>
                     </div>
+                    {emailError && <div className="error">{emailError}</div>}
                     <div className="newsletter__email">
-                        {emailError && <div className="error">{emailError}</div>}
                         <input 
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
